abe542d46eb2f3a3fdd420976ab0fe7c5917e7ea
Jeremy Silver <jeremy@udacity.com>
2015-08-02 17:03:23 -0700

    Add initial line drawing and icicles projects

---
+++ Icicles/html/src/com/udacity/gamedev/Icicles/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.udacity.gamedev.Icicles.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.udacity.gamedev.Icicles.IciclesGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new IciclesGame();
+        }
+}
