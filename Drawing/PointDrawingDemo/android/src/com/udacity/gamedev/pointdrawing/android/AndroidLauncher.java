2f058135badc496d40c42cb2e729136f56a5023b
Jeremy Silver <jeremy@udacity.com>
2015-08-05 17:11:00 -0700

    Start again

---
+++ Drawing/PointDrawingDemo/android/src/com/udacity/gamedev/pointdrawing/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.udacity.gamedev.pointdrawing.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.udacity.gamedev.pointdrawing.PointDrawing;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PointDrawing(), config);
+	}
+}
---
+++ Drawing/PointDrawingDemo/html/src/com/udacity/gamedev/pointdrawing/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.udacity.gamedev.pointdrawing.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.udacity.gamedev.pointdrawing.PointDrawing;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new PointDrawing();
+        }
+}
