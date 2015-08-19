47407d397f97f32656435da81f85700dce2ec5a0
Jeremy Silver <jeremy@udacity.com>
2015-08-19 16:54:26 -0700

    Add a camera to the screensaver

--- ScreenSaver/core/src/com/udacity/gamedev/ScreenSaver/BouncingBall.java
+++ ScreenSaver/core/src/com/udacity/gamedev/ScreenSaver/BouncingBall.java
@@ -1,5 +1,8 @@
 package com.udacity.gamedev.ScreenSaver;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -9,12 +12,46 @@ public class BouncingBall {
 
     Vector2 position;
     Vector2 velocity;
-    int screenWidth;
-    int screenHeight;
+    float radius;
 
-    public void update(float delta){
-
+    public BouncingBall(){
+        init();
     }
 
-    private void collide
+    private void init(){
+        position = new Vector2(5,5);
+        velocity = new Vector2(5, 5);
+        radius = .5f;
+    }
+
+    public void update(float delta, float viewportWidth, float viewportHeight){
+        position.x += delta * velocity.x;
+        position.y += delta * velocity.y;
+        collideWithWalls(viewportWidth, viewportHeight);
+    }
+
+    private void collideWithWalls(float viewportWidth, float viewportHeight){
+        if (position.x - radius < 0){
+            position.x = radius;
+            velocity.x = -velocity.x;
+        }
+        if (position.x + radius > viewportWidth){
+            position.x = viewportWidth - radius;
+            velocity.x = -velocity.x;
+        }
+        if (position.y - radius < 0){
+            position.y = radius;
+            velocity.y = -velocity.y;
+        }
+        if (position.y + radius > viewportHeight){
+            position.y = viewportHeight - radius;
+            velocity.y = -velocity.y;
+        }
+    }
+
+    public void render(ShapeRenderer shapeRenderer){
+        shapeRenderer.set(ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.circle(position.x, position.y, radius, 20);
+    }
 }
--- ScreenSaver/core/src/com/udacity/gamedev/ScreenSaver/ScreenSaverScreen.java
+++ ScreenSaver/core/src/com/udacity/gamedev/ScreenSaver/ScreenSaverScreen.java
@@ -1,8 +1,12 @@
 package com.udacity.gamedev.ScreenSaver;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 /**
  * Created by silver on 8/19/15.
@@ -10,20 +14,37 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class ScreenSaverScreen extends InputAdapter implements Screen {
 
     ShapeRenderer shapeRenderer;
+    BouncingBall bouncingBall;
+    OrthographicCamera camera;
+
+    private static final float WORLD_SMALLEST_DIMENSION = 10.0f;
 
     @Override
     public void show() {
         shapeRenderer = new ShapeRenderer();
+        bouncingBall = new BouncingBall();
+        camera = new OrthographicCamera();
     }
 
     @Override
     public void render(float delta) {
-
+        camera.update();
+        bouncingBall.update(delta, camera.viewportWidth, camera.viewportHeight);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        shapeRenderer.begin(ShapeType.Filled);
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        bouncingBall.render(shapeRenderer);
+        shapeRenderer.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        if (width > height){
+            camera.setToOrtho(false, WORLD_SMALLEST_DIMENSION * width/height, WORLD_SMALLEST_DIMENSION);
+        } else {
+            camera.setToOrtho(false, WORLD_SMALLEST_DIMENSION, WORLD_SMALLEST_DIMENSION * height / width);
+        }
     }
 
     @Override
