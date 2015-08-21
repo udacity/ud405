999fd802e06e24cdd6f2cb4fdb316a9af9fa0697
Jeremy Silver <jeremy@udacity.com>
2015-08-21 00:49:27 -0700

    Add initial modern art solution

---
+++ 1.3.09-Solution-CreateModernArt/desktop/src/com/udacity/gamedev/modernart/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.udacity.gamedev.modernart.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.udacity.gamedev.modernart.ModernArt;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ModernArt(), config);
+	}
+}
