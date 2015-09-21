40c498ca5b30e46ea8e854ae18e8137f529ce40f
Jeremy Silver <jeremy@udacity.com>
2015-09-21 10:32:49 -0700

    Add initial circles and arcs demo

---
+++ 1.4-Development/CirclesAndArcs/desktop/src/com/udacity/gamedev/circlesandarcs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.udacity.gamedev.circlesandarcs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.udacity.gamedev.circlesandarcs.CirclesAndArcs;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CirclesAndArcs(), config);
+	}
+}
