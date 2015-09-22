c23561fc67519d37016294d1b1003c9db55afd52
Jeremy Silver <jeremy@udacity.com>
2015-09-22 12:09:46 -0700

    Complete stick figure solution

--- 1.4-Development/StickFigureSolution/core/src/com/udacity/gamedev/stickfigure/StickFigure.java
+++ 1.4-Development/StickFigureSolution/core/src/com/udacity/gamedev/stickfigure/StickFigure.java
@@ -3,25 +3,56 @@ package com.udacity.gamedev.stickfigure;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
+/**
+ * TODO: Start here
+ *
+ * In this exercise you'll set up a ShapeRenderer, and use it to draw a stick figure. At minimum,
+ * you'll need a circle for the head, and five lines for the torso, arms, and legs.
+ *
+ * Remember to set up a ShapeRenderer you'll need to declare it, initialize it, and dispose of it.
+ * Then to actually use the ShapeRenderer you'll need to start a batch of the appropriate type, draw
+ * your shapes, and end the batch.
+ *
+ * We don't have step-by-step TODOs for this one, since the aim is for you to remember the steps for
+ * setting up a ShapeRenderer and be able to set one up on your own. Of course, the solution is
+ * still available, if you run into anything confusing.
+ */
 public class StickFigure extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    ShapeRenderer renderer;
+
+    @Override
+    public void create() {
+        renderer = new ShapeRenderer();
+    }
+
+    @Override
+    public void dispose() {
+        renderer.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.begin(ShapeType.Filled);
+        // Head
+        renderer.circle(100, 100, 10);
+        renderer.end();
+
+        renderer.begin(ShapeType.Line);
+        // Torso
+        renderer.line(100, 50, 100, 100);
+        // Legs
+        renderer.line(85, 35, 100, 50);
+        renderer.line(115, 35, 100, 50);
+        // Arms
+        renderer.line(85, 70, 100, 85);
+        renderer.line(115, 70, 100, 85);
+        renderer.end();
+    }
 }
