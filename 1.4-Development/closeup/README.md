f9e0ba7a46fb0ea8798ebe90d8c918d5cbb50917
Jeremy Silver <jeremy@udacity.com>
2015-09-21 17:46:07 -0700

    Finish solar system demo

---
+++ 1.4-Development/closeup/README.md
@@ -0,0 +1,5 @@
+# Working with an OrthographicCamera
+
+Now that we've seen what an OrthographicCamera can do for us, let's see how to create, manipulate, and apply one!
+
+Check out the TODOs to get started.
--- 1.4-Development/closeup/core/src/com/udacity/gamedev/closeup/Closeup.java
+++ 1.4-Development/closeup/core/src/com/udacity/gamedev/closeup/Closeup.java
@@ -26,7 +26,6 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
  * area on the screen the camera will draw to.
  */
 
-
 public class Closeup extends ApplicationAdapter {
 
     public static final String TAG = Closeup.class.getName();
@@ -96,8 +95,9 @@ public class Closeup extends ApplicationAdapter {
         // Draw the satellite
         renderer.setColor(Color.RED);
 
-        renderer.rect(SATELLITE_POSITION_X, SATELLITE_POSITION_Y,
-                SATELLITE_SIZE / 2, SATELLITE_SIZE / 2,
+        float halfSize = SATELLITE_SIZE / 2;
+        renderer.rect(SATELLITE_POSITION_X - halfSize, SATELLITE_POSITION_Y - halfSize,
+                halfSize, halfSize,
                 SATELLITE_SIZE, SATELLITE_SIZE, 1, 1, SATELLITE_ROTATION);
 
         renderer.end();
@@ -114,15 +114,16 @@ public class Closeup extends ApplicationAdapter {
      * the world will appear tiny.
      *
      * Here we've factored out the code needed to get the camera to focus on each of the solar
-     * system objects in turn.
+     * system objects in turn. Uncomment the code for the object you want to focus on.
      */
 
     @Override
     public void resize(int width, int height) {
         float aspectRatio = 1.0f * width / height;
-        trackStar(camera, aspectRatio);
+        camera.setToOrtho(false, width, height);
+//        trackStar(camera, aspectRatio);
 //        trackPlanet(camera, aspectRatio);
-//        trackSatellite(camera, aspectRatio);
+        trackSatellite(camera, aspectRatio);
     }
 
 
@@ -130,30 +131,58 @@ public class Closeup extends ApplicationAdapter {
      * To get the camera to zoom out, so we can see the whole star, first we need to set the
      * camera's viewport size using setToOrtho. The first argument is whether we want increasing Y
      * to mean moving down. We don't, so we'll set it to false. The next two arguments are the
-     * viewport width and height. The height just needs to be double the radius of the star, but if we set the viewport width to be equal to the viewport height, the star will be a stretched out oval, instead of a circle.
+     * viewport width and height. The height just needs to be double the radius of the star, but if
+     * we set the viewport width to be equal to the viewport height, the star will be a stretched
+     * out oval, instead of a circle.
      *
-     * This is because the aspect ratio (width / height) of the camera doesn't match the aspect ratio of the screen. We can fix this by making our viewport wider, by multiplying twice the star radius by the aspect ratio.
+     * This is because the aspect ratio (width / height) of the camera doesn't match the aspect
+     * ratio of the screen. We can fix this by making our viewport wider, by multiplying twice the
+     * star radius by the aspect ratio.
      *
-     * If we stop there, it looks like our camera viewport is now big enough to hold the star, but we're not looking at the right spot. In fact, it looks like the camera's bottom left corner is still in exactly the
+     * If we stop there, it looks like our camera viewport is now big enough to hold the star, but
+     * we're not looking at the right spot. In fact, it looks like the is centered in exactly the
+     * same location as before. Let's set the camera's position to the center of the star.
+     *
+     * That looks pretty good, but now we've lost the planet. Let's use translate to nudge the
+     * camera to the right, so we can see the planet again.
      */
     private void trackStar(OrthographicCamera camera, float aspectRatio) {
-
-        // This makes the star look stretched out, since the viewport is the same
-        //camera.setToOrtho(false, 2 * STAR_RAIDUS, 2 * STAR_RAIDUS);
-
-        camera.setToOrtho(false, 2 * STAR_RAIDUS * aspectRatio, 2 * STAR_RAIDUS);
-        camera.translate(STAR_CENTER_X - STAR_RAIDUS, STAR_CENTER_Y - STAR_RAIDUS);
+        camera.viewportHeight = 2 * STAR_RAIDUS;
+        // This will stretch the star, since the aspect ratio doesn't match
+//        camera.viewportWidth = 2 * STAR_RAIDUS;
+        camera.viewportWidth = aspectRatio * camera.viewportHeight;
+        camera.position.set(STAR_CENTER_X, STAR_CENTER_Y, 0);
+        camera.translate(STAR_RAIDUS * (aspectRatio - 1), 0);
     }
 
+    /**
+     * Let's move the camera to focus on the green planet. First, we need to set the viewport size,
+     * as we did with the star. Now we need to move the camera to the planet.
+     *
+     * A different way to do that is to move the camera to the origin, then translate it to the
+     * position of the planet.
+     */
     private void trackPlanet(OrthographicCamera camera, float aspectRatio) {
-        camera.setToOrtho(false, 2 * PLANET_RADIUS * aspectRatio, 2 * PLANET_RADIUS);
-        camera.translate(PLANET_CENTER_X - PLANET_RADIUS * aspectRatio, PLANET_CENTER_Y - PLANET_RADIUS);
+        camera.viewportHeight = 2 * PLANET_RADIUS;
+        camera.viewportWidth = aspectRatio * camera.viewportHeight;
+        camera.position.set(0, 0, 0);
+        camera.translate(PLANET_CENTER_X, PLANET_CENTER_Y, 0);
     }
+
+    /**
+     * Finally, let's zoom in on the satellite, and match its rotation. This could be very tricky,
+     * given that rectangles are drawn by specifying their lower left corner, and also considering
+     * that rectangles can specify the origin about which they will be rotated.
+     *
+     * Fortunately, we were smart when drawing the satellite, and made sure that it was _centered_ at
+     * SATELLITE_POSITION_X, SATELLITE_POSITION_Y, and also rotated about that point. That makes our
+     * job super easy. We just move the camera to that position, and rotate the camera.
+     */
 
     private void trackSatellite(OrthographicCamera camera, float aspectRatio) {
-        camera.setToOrtho(false, SATELLITE_SIZE * aspectRatio, SATELLITE_SIZE);
-
-        camera.translate(SATELLITE_POSITION_X - SATELLITE_SIZE / 2 * (aspectRatio - 1), SATELLITE_POSITION_Y);
+        camera.viewportHeight = 2 * SATELLITE_SIZE;
+        camera.viewportWidth = aspectRatio * camera.viewportHeight;
+        camera.position.set(SATELLITE_POSITION_X, SATELLITE_POSITION_Y, 0);
         camera.rotate(SATELLITE_ROTATION);
     }
 }
