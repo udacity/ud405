9c68cdc0b68bfb95eb67d85ab19d385e84f5ed32
Jeremy Silver <jeremy@udacity.com>
2015-09-23 01:43:04 -0700

    Add Cyclic Overlap Solution

---
+++ 1.4-Development/CyclicOverlapSolution/desktop/src/com/udacity/gamedev/cyclicoverlap/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.udacity.gamedev.cyclicoverlap.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.udacity.gamedev.cyclicoverlap.CyclicOverlap;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CyclicOverlap(), config);
+	}
+}
