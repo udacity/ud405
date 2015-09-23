8e65148fddb40b4ee3968413edfb423f987510cb
Jeremy Silver <jeremy@udacity.com>
2015-09-23 02:01:24 -0700

    Finish Smiley face solution

--- 1.4-Development/smileyface/core/src/com/udacity/gamedev/smileyface/SmileyFace.java
+++ 1.4-Development/smileyface/core/src/com/udacity/gamedev/smileyface/SmileyFace.java
@@ -11,7 +11,12 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
 /**
  * TODO: Start here
  *
- * The goal of this exercise is just to draw a simily face. Feel free to get that done however you
+ * The goal of this exercise is just to draw a similey face. Feel free to get as artistic as you
+ * want, but we've provided a bunch of constants you may find useful.
+ *
+ * The tricky part is drawing the mouth. Since we can't draw thick lines, making a thick line for
+ * the mouth is hard. The trick is to draw two arcs. One black one, then a slightly smaller yellow
+ * one. The portion of the black arc that isn't covered by the yellow arc becomes the mouth.
  */
 
 public class SmileyFace extends ApplicationAdapter {
@@ -32,44 +37,46 @@ public class SmileyFace extends ApplicationAdapter {
     static final int EYE_SEGMENTS = 20;
     static final int MOUTH_SEGMENTS = 20;
 
-    // TODO: Declare
+    // TODO: Declare a ShapeRenderer and an ExtendViewport
     ShapeRenderer renderer;
     ExtendViewport viewport;
 
     @Override
     public void create() {
+        // TODO: Initialize the ShapeRenderer and ExtendViewport
         renderer = new ShapeRenderer();
         viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT);
     }
 
     @Override
     public void dispose() {
+        // TODO: Dispose of the ShapeRenderer
         renderer.dispose();
     }
 
     @Override
     public void resize(int width, int height) {
+        // TODO: Update the viewport
         viewport.update(width, height, true);
-        viewport.getCamera().position.set(FACE_CENTER_X, FACE_CENTER_Y,0);
+        // TODO: Move the viewport's camera to the center of the face
+        viewport.getCamera().position.set(FACE_CENTER_X, FACE_CENTER_Y, 0);
     }
 
     /**
      * We'll often want to break up our drawing into separate functions, or different objects
-     * entirely. This is easy to do, all we need to do is pass in our ShapeRenderer. All we need to do here is
+     * entirely. This is easy to do, all we need to do is pass in our ShapeRenderer.
      */
-
     @Override
     public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        // TODO: Apply the viewport
         viewport.apply();
+        // TODO: Set the ShapeRender's projection matrix
         renderer.setProjectionMatrix(viewport.getCamera().combined);
         renderer.begin(ShapeType.Filled);
         drawSmileyFace(renderer);
         renderer.end();
-
-
     }
 
     private void drawSmileyFace(ShapeRenderer renderer) {
@@ -80,10 +87,10 @@ public class SmileyFace extends ApplicationAdapter {
         renderer.setColor(Color.BLACK);
         renderer.circle(FACE_CENTER_X - EYE_OFFSET, FACE_CENTER_Y + EYE_OFFSET, EYE_RADIUS, EYE_SEGMENTS);
         renderer.circle(FACE_CENTER_X + EYE_OFFSET, FACE_CENTER_Y + EYE_OFFSET, EYE_RADIUS, EYE_SEGMENTS);
+        // TODO: Draw a black arc for the mouth (Hint: MOUTH_OUTER_RADIUS)
         renderer.arc(FACE_CENTER_X, FACE_CENTER_Y, MOUTH_OUTER_RADIUS, MOUTH_START_ANGLE, MOUTH_DEGREES, MOUTH_SEGMENTS);
+        // TODO: Draw a yellow arc to make the mouth actually look like a mouth (Hint: MOUTH_INNER_RADIUS)
         renderer.setColor(Color.YELLOW);
         renderer.arc(FACE_CENTER_X, FACE_CENTER_Y, MOUTH_INNER_RADIUS, MOUTH_START_ANGLE, MOUTH_DEGREES, MOUTH_SEGMENTS);
     }
-
-
 }
