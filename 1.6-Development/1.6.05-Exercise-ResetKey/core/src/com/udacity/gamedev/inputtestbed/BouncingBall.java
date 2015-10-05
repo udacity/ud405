6dd308c3015fe36d3d5fc6215062801fa32f7a81
Jeremy Silver <jeremy@udacity.com>
2015-10-05 10:39:32 -0700

    Finish 1.6.05

--- 1.6-Development/1.6.05-Exercise-ResetKey/core/src/com/udacity/gamedev/inputtestbed/BouncingBall.java
+++ 1.6-Development/1.6.05-Exercise-ResetKey/core/src/com/udacity/gamedev/inputtestbed/BouncingBall.java
@@ -127,9 +127,7 @@ public class BouncingBall extends InputAdapter {
         }
 
         // TODO: If Keys.R was pressed, call init() to reset the ball
-        if (keycode == Keys.R){
-            init();
-        }
+
 
         return true;
     }
