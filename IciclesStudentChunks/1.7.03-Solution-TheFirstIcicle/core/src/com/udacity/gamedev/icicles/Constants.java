0f82e83f70e4bbfaff116b1790d16f898f3c5d76
Jeremy Silver <jeremy@udacity.com>
2015-10-13 23:24:33 -0700

    Add first icicle solution

---
+++ IciclesStudentChunks/1.7.03-Solution-TheFirstIcicle/core/src/com/udacity/gamedev/icicles/Constants.java
@@ -0,0 +1,13 @@
+package com.udacity.gamedev.icicles;
+
+import com.badlogic.gdx.graphics.Color;
+
+
+public class Constants {
+    public static final float WORLD_SIZE = 10.0f;
+    public static final Color BACKGROUND_COLOR = Color.BLUE;
+
+    public static final float ICICLES_HEIGHT = 1.0f;
+    public static final float ICICLES_WIDTH = 0.5f;
+    public static final Color ICICLE_COLOR = Color.WHITE;
+}
---
+++ IciclesStudentChunks/1.7.03-Solution-TheFirstIcicle/core/src/com/udacity/gamedev/icicles/Player.java
@@ -0,0 +1,7 @@
+package com.udacity.gamedev.icicles;
+
+public class Player {
+
+    public static final String TAG = Player.class.getName();
+
+}
