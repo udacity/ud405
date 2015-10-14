50395023d4104809119c83e19d2db20d031a73b7
Jeremy Silver <jeremy@udacity.com>
2015-10-13 23:22:31 -0700

    Add draw the player solution

---
+++ IciclesStudentChunks/1.7.04-Solution-DrawThePlayer/core/src/com/udacity/gamedev/icicles/Constants.java
@@ -0,0 +1,20 @@
+package com.udacity.gamedev.icicles;
+
+import com.badlogic.gdx.graphics.Color;
+
+
+public class Constants {
+    public static final float WORLD_SIZE = 10.0f;
+    public static final Color BACKGROUND_COLOR = Color.BLUE;
+
+    public static final float PLAYER_HEAD_RADIUS = 0.5f;
+    public static final float PLAYER_HEAD_HEIGHT = 4.0f * PLAYER_HEAD_RADIUS;
+    public static final float PLAYER_LIMB_WIDTH = 0.1f;
+    public static final int PLAYER_HEAD_SEGMENTS = 20;
+    public static final Color PLAYER_COLOR = Color.BLACK;
+
+    public static final float ICICLES_HEIGHT = 1.0f;
+    public static final float ICICLES_WIDTH = 0.5f;
+    public static final Color ICICLE_COLOR = Color.WHITE;
+
+}
